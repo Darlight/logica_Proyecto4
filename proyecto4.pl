ce22dcd4c7783b990bf829028b905df4c2261fd5
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-11-12 11:41:37 -0600

    Update proyecto4.pl

--- proyecto4.pl
+++ proyecto4.pl
@@ -4,6 +4,12 @@ Juan Diego Solorzano
 Mario Perdomo
 Josue Sagastume
 */
+%Nodos
+nodo(a).
+nodo(b).
+nodo(c).
+nodo(d).
+
 %Se determinan las aristas del grafo
 arco(a,b).
 arco(a,c).
@@ -18,5 +24,5 @@ camino(X, X, _, []).
 %Buscar en otros nodos
 camino(X, Y, Visitados, [Z|Cola]) :-
        arco(X,Z),
-       \+member(Z, Visitados), %Asegurar que no esta en visitados
+       \+(member(Z, Visitados)), %Asegurar que no esta en visitados
        camino(Z,Y,[Z|Visitados],Cola). %Agregar Z al camino
